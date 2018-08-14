48f35aa3c8852682ffca52671c533e793a991f18
victordroid <victor.gea@gmail.com>
2018-08-14 10:44:03 +0200

    Initial commit

---
+++ src/main/java/appiumIlusia/Plantilla/App.java
@@ -0,0 +1,13 @@
+package appiumIlusia.Plantilla;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
